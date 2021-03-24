070eef7b4579dc5696af2b2c5e37f4a9e8cccd71
rubenSinzig <ruben.sinzig@posteo.de>
2021-03-24 20:14:44 +0100

    mobile -> welcome & menu

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ document.querySelector(".hamburger").addEventListener("click", () => {
   clicked = !clicked;
   if (clicked) {
     document.querySelector(".burger-top").style.cssText =
-      "position: absolute; transform: rotate(-0.15turn); transition: 0.6s; width: 1.4rem;";
+      "top: 1.8rem; transform: rotate(-0.125turn); transition: 0.6s; width: 1.25rem";
     document.querySelector(".burger-bottom").style.cssText =
-      "position: relative; transform: rotate(0.15turn); transition: 0.6s; width: 1.4rem;";
-    document.querySelector(".nav").style.cssText =
-      "display: block; position: absolute; top: 7vh;";
+      "top: 1.8rem; transform: rotate(0.125turn); transition: 0.6s; width: 1.25rem";
+    document.querySelector(".nav").style.cssText = "display: block; top: 7vh;";
   } else {
-    document.querySelector(".burger-top").style.cssText = "position: static;";
+    document.querySelector(".burger-top").style.cssText =
+      "position: absolute; transition: 0.6s;";
     document.querySelector(".burger-bottom").style.cssText =
-      "position: static;";
+      "position: absolute; transition: 0.6s;";
     document.querySelector(".nav").style.cssText = "display: none;";
   }
 });
