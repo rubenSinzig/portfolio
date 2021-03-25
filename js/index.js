b26fc3fd2934000714d47bde74b8db9ca7ea99ca
rubenSinzig <ruben.sinzig@posteo.de>
2021-03-25 11:55:51 +0100

    mobile -> about section

--- js/index.js
+++ js/index.js
@@ -19,9 +19,15 @@ document.querySelector(".hamburger").addEventListener("click", () => {
 document.querySelector(".left-top").addEventListener("click", () => {
   clicked = !clicked;
   if (clicked) {
+    document.querySelector(".x-top").style.cssText =
+      "position: relative; top: 0.4rem; transform: rotate(-0.125turn); transition: 1s;";
+    document.querySelector(".x-bottom").style.cssText =
+      "position: relative; transform: rotate(0.125turn); transition: 1s;";
     document.querySelector(".top-left").style.cssText =
       "position: relative; top: 12rem; left: 5.8rem; transform: scale(1.8); box-shadow: 0 0 0.5rem wheat;";
   } else {
+    document.querySelector(".x-top").style.cssText = "position: static;";
+    document.querySelector(".x-bottom").style.cssText = "position: static;";
     document.querySelector(".top-left").style.cssText = "position: static;";
   }
 });
@@ -29,9 +35,15 @@ document.querySelector(".left-top").addEventListener("click", () => {
 document.querySelector(".right-top").addEventListener("click", () => {
   clicked = !clicked;
   if (clicked) {
+    document.querySelector(".xx-top").style.cssText =
+      "position: relative; top: 0.4rem; transform: rotate(-0.125turn); transition: 1s;";
+    document.querySelector(".xx-bottom").style.cssText =
+      "position: relative; transform: rotate(0.125turn); transition: 1s;";
     document.querySelector(".top-right").style.cssText =
       "position: relative; top: 12rem; left: -5.8rem; transform: scale(1.8); box-shadow: 0 0 0.5rem wheat;";
   } else {
+    document.querySelector(".xx-top").style.cssText = "position: static;";
+    document.querySelector(".xx-bottom").style.cssText = "position: static;";
     document.querySelector(".top-right").style.cssText = "position: static;";
   }
 });
@@ -39,9 +51,15 @@ document.querySelector(".right-top").addEventListener("click", () => {
 document.querySelector(".left-bottom").addEventListener("click", () => {
   clicked = !clicked;
   if (clicked) {
+    document.querySelector(".xxx-top").style.cssText =
+      "position: relative; top: 0.4rem; transform: rotate(-0.125turn); transition: 1s;";
+    document.querySelector(".xxx-bottom").style.cssText =
+      "position: relative; transform: rotate(0.125turn); transition: 1s;";
     document.querySelector(".bottom-left").style.cssText =
       "position: relative; top: -11rem; left: 5.8rem; transform: scale(1.8); box-shadow: 0 0 0.5rem wheat;";
   } else {
+    document.querySelector(".xxx-top").style.cssText = "position: static;";
+    document.querySelector(".xxx-bottom").style.cssText = "position: static;";
     document.querySelector(".bottom-left").style.cssText = "position: static;";
   }
 });
@@ -49,9 +67,15 @@ document.querySelector(".left-bottom").addEventListener("click", () => {
 document.querySelector(".right-bottom").addEventListener("click", () => {
   clicked = !clicked;
   if (clicked) {
+    document.querySelector(".xxxx-top").style.cssText =
+      "position: relative; top: 0.4rem; transform: rotate(-0.125turn); transition: 1s;";
+    document.querySelector(".xxxx-bottom").style.cssText =
+      "position: relative; transform: rotate(0.125turn); transition: 1s;";
     document.querySelector(".bottom-right").style.cssText =
       "position: relative; top: -11rem; left: -5.8rem; transform: scale(1.8); box-shadow: 0 0 0.5rem wheat;";
   } else {
+    document.querySelector(".xxxx-top").style.cssText = "position: static;";
+    document.querySelector(".xxxx-bottom").style.cssText = "position: static;";
     document.querySelector(".bottom-right").style.cssText = "position: static;";
   }
 });
